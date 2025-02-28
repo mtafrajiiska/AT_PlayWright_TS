6b7ca2cf96601056c159f5e5ba1037ac1ee383c8
Mirena Tafrajiiska (Crossroad) <mtafrajiiska@crossroad.bg>
2025-02-28 20:04:38 +0200

    Commit Lecture Arrays&Functions

--- index.ts
+++ index.ts
@@ -43,4 +43,48 @@ stringValue = 10;
 stringValue = 10===10;
 stringValue = [10,20,30];
 
-console.log ("stringValue", stringValue);
+// 27.02.2025 
+// Ternary operator
+const isPositiveNum: string = 
+5>0? "The number is positive" : "The number is negative";
+
+const age: number = 30;
+const myName: string = "Pesho";
+
+const personDescription = 
+age < 40
+ ? `${myName} is younger than 40. He is ${age} years old.`
+ : `${myName} is older than 40. He is ${age} years old.`;
+
+ const personName = `His name is "${myName}".`;
+
+ console.log("personName", personName);
+
+// If Else Statement
+
+if (age < 40) {
+    console.log("Age are lower than 40");
+}else{
+    console.log("Age are greater than 40");
+}
+
+const numm: number = 100;
+
+if (numm <=10){
+    console.log("The number is lower than 10.");
+    } else if (numm > 80) {
+    console.log("The number is greater than 80 and less than 100.");
+    } else if (numm >= 100) {
+     console.log("The number is greater than 100.");   
+    } else {
+        console.log("The number is between 10 and 100.");
+    }
+
+
+
+
+// Arrays
+//type PersonInfoType = string | number;
+
+//const personNameInf: PersonInfoType = "Ivan";
+//const personNameInf: PersonInfoType = 25;
