a171ab316e515af59c61772110a27a0e8209ade8
Mirena Tafrajiiska (Crossroad) <mtafrajiiska@crossroad.bg>
2025-03-13 18:54:26 +0200

    Dom-Manipulations-Homework

---
+++ dom-manipulations/dom-manipulation-homework.ts
@@ -0,0 +1,11 @@
+const headingsOne: HTMLHeadingElement = document.getElementsByTagName("h1")[0];
+
+//
+headingsOne.textContent = "Welcome to TypeScript Dom Manipulation Homework";
+
+const divContent = document.querySelector("div") as HTMLDivElement;
+divContent.style.color = "blue";
+
+
+//const listItem = document.getElementsByClassName("li")[0,1,2];
+
